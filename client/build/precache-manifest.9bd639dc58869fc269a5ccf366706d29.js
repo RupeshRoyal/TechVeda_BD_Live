3239eb59d757743b76ae17ebd1752a0c987031c0
RupeshRoyal <roopeshroyal34@gmail.com>
2024-07-07 22:33:31 +0530

    first commit

---
+++ client/build/precache-manifest.9bd639dc58869fc269a5ccf366706d29.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4082a81312b2b2aab6dc70ce67c64a01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "283830dad7e827ec0217",
+    "url": "/static/css/2.e34ea60e.chunk.css"
+  },
+  {
+    "revision": "19a836aa0364f7c21ff8",
+    "url": "/static/css/main.68e0d14d.chunk.css"
+  },
+  {
+    "revision": "283830dad7e827ec0217",
+    "url": "/static/js/2.4a347e85.chunk.js"
+  },
+  {
+    "revision": "c0a8983e7b04b55097f0a5829b8ecdbe",
+    "url": "/static/js/2.4a347e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a836aa0364f7c21ff8",
+    "url": "/static/js/main.747d59be.chunk.js"
+  },
+  {
+    "revision": "4a22b31186717ff17b9e",
+    "url": "/static/js/runtime-main.4d1956cb.js"
+  },
+  {
+    "revision": "e12a9bc9839607a35216b78d59da8a68",
+    "url": "/static/media/Compare_with_others.e12a9bc9.png"
+  },
+  {
+    "revision": "866a5b613ca80a3b5d81c6b7bcc50227",
+    "url": "/static/media/FoundingStory.866a5b61.png"
+  },
+  {
+    "revision": "3b8c9d38e77e5fb91ad88928ee736412",
+    "url": "/static/media/Instructor.3b8c9d38.png"
+  },
+  {
+    "revision": "64f89ae7d04536dd8d9cfdf336295a2a",
+    "url": "/static/media/Know_your_progress.64f89ae7.png"
+  },
+  {
+    "revision": "4f80109bb1a9e5ad34ea3a1ae6435cbb",
+    "url": "/static/media/Logo1.4f80109b.svg"
+  },
+  {
+    "revision": "958f7ab2900a9c3f6df910c0c301cbc6",
+    "url": "/static/media/Logo2.958f7ab2.svg"
+  },
+  {
+    "revision": "9410a5f225f0fadc7e59c15a4ae016ab",
+    "url": "/static/media/Logo3.9410a5f2.svg"
+  },
+  {
+    "revision": "4cd16dddab492df7d09ce7046de38148",
+    "url": "/static/media/Logo4.4cd16ddd.svg"
+  },
+  {
+    "revision": "ee27d0d886834149fe60dc3519194c19",
+    "url": "/static/media/Plan_your_lessons.ee27d0d8.png"
+  },
+  {
+    "revision": "11741e47589b2a34924e587bc3a53842",
+    "url": "/static/media/TimelineImage.11741e47.png"
+  },
+  {
+    "revision": "2e4fc28580cf6f01870bfcca1d045495",
+    "url": "/static/media/aboutus1.2e4fc285.webp"
+  },
+  {
+    "revision": "9ae6066a2ce472032574314dac57d640",
+    "url": "/static/media/aboutus2.9ae6066a.webp"
+  },
+  {
+    "revision": "febfcc9aff85eb221d150067902f50d8",
+    "url": "/static/media/aboutus3.febfcc9a.webp"
+  },
+  {
+    "revision": "9c814bc2407d44d9236f7b27d80fecb1",
+    "url": "/static/media/banner.9c814bc2.mp4"
+  },
+  {
+    "revision": "f7dc7ebbf956b22e4721fae24d0c74ea",
+    "url": "/static/media/bghome.f7dc7ebb.svg"
+  },
+  {
+    "revision": "ea07c2d4f57c86dffbf2c85e04686967",
+    "url": "/static/media/frame.ea07c2d4.png"
+  },
+  {
+    "revision": "4098c4502d5392e56544eb4377103727",
+    "url": "/static/media/login.4098c450.webp"
+  },
+  {
+    "revision": "209550a04072186b5ae1b5886813b457",
+    "url": "/static/media/rzp_logo.209550a0.png"
+  },
+  {
+    "revision": "1af9406b6989aaf47e62926952997b40",
+    "url": "/static/media/signup.1af9406b.webp"
+  }
+]);
